bc6bf0f0ff359080fed2dc4797eff95ebf232e7d
muhammedsamsad <mskp.shamshad@gmail.com>
2022-01-15 02:27:30 +0300

    updtes in styles

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,15 @@ from accounts.models import *
 
 
 class AgentForm(forms.ModelForm):
+    languages = forms.ModelMultipleChoiceField(
+        queryset=Spoken_Languages.objects.all(),
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'class': 'form-control form-check form-check-inline'})  #need to check later
+            
+
+
+
+    )
 
     class Meta:
         model = Agent
