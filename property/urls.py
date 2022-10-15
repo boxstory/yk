9ee2ce04aa7e72e91545eb6251a7d7170d4d30ee
boxstory <boxstory.clt@gmail.com>
2022-10-15 13:31:21 +0300

    url update

--- property/urls.py
+++ property/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('<int:pk>/', views.own_properties, name='own_properties'),
     path('<int:pk>/add/', views.property_create, name='property_create'),
     path('<int:pk>/update/', views.property_update, name='property_update'),
-    path('<int:pk>/<property_id>/', views.single_property, name='single_property'),
+    path('<int:pk>/<int:property_id>/',
+         views.single_property, name='single_property'),
 
 ]
